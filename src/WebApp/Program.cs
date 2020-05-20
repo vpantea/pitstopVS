d0e094faa5709bde0a9e67104ed78109b052f945
vpantea <vpantea@gmail.com>
2020-05-20 20:51:45 +0300

    local pitstop in VS, no container yet

---
+++ src/WebApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+
+namespace PitStop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseSerilog()
+                .UseHealthChecks("/hc")
+                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory());
+    }
+}
